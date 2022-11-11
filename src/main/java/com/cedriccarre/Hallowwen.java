60474d905115d4c0d939486f20712c6d1e35937f
Cédric Carré <cedric.carre@arolla.fr>
2022-11-11 16:15:23 +0100

    Break

--- src/main/java/com/cedriccarre/Hallowwen.java
+++ src/main/java/com/cedriccarre/Hallowwen.java
@@ -1,18 +1,37 @@
 package com.cedriccarre;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Hallowwen {
-    public static String givePacket(int nbOfChildren, List<String[]> packets) {
-        if(packets.size() < nbOfChildren) {
+    public static String givePacket(int nbOfChildren, List<List<String>> bag) {
+        List<List<String>> newBag = new ArrayList<>();
+        if(bag.size() < nbOfChildren) {
             return "Trick or treat!";
         }
-        for (String [] packet: packets) {
-            for (int i = 0; i < packet.length; i++) {
-                if(!Objects.equals(packet[i], "candy")){
+        for (List<String> packet: bag) {
+            List<String> filteredPacked = new ArrayList<>();
+            for (int i = 0; i < packet.size(); i++) {
+                if(packet.size() < 2){
                     return "Trick or treat!";
                 }
+                if(packet.get(i).equals("bomb")){
+                    return "Trick or treat!";
+                }
+                if(packet.get(i).equals("candy")){
+                    filteredPacked.add(packet.get(i));
+                }
+            }
+            newBag.add(filteredPacked);
+        }
+        List<Integer> candieNbPerPacket = new ArrayList<>();
+        for (List<String> packet : newBag) {
+            candieNbPerPacket.add(packet.size());
+        }
+        for (int i = 0; i < candieNbPerPacket.size(); i++) {
+            if(candieNbPerPacket.get(0) != candieNbPerPacket.get(i)){
+                return "Trick or treat!";
             }
         }
         return "Thank you, strange uncle!";
--- src/test/java/HalloweenShould.java
+++ src/test/java/HalloweenShould.java
@@ -10,23 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class HalloweenShould {
 
     @Test
-    void todo() {
-        //Arrange
-        List<String[]> packets = new ArrayList<>();
-        String[] p1 = {"candy", "candy"};
-        packets.add(p1);
-        //Act
-        String actualResult = Hallowwen.givePacket(1 , packets);
-        //Assert
-        assertThat(actualResult).isEqualTo("Thank you, strange uncle!");
-    }
-
-    @Test
     void return_thx_when_given_2_children_2_packets_with_2_candies_in_each() {
         //Arrange
-        List<String[]> packets = new ArrayList<>();
-        String[] p1 = {"candy", "candy"};
-        String[] p2 = {"candy", "candy"};
+        List<List<String>> packets = new ArrayList<>();
+        List<String> p1 = List.of("candy", "candy");
+        List<String> p2 = List.of("candy", "candy");
         packets.add(p1);
         packets.add(p2);
         //Act
@@ -37,9 +25,9 @@ public class HalloweenShould {
     @Test
     void return_trick_when_given_3_children_only_2_packets() {
         //Arrange
-        List<String[]> packets = new ArrayList<>();
-        String[] p1 = {"candy", "candy"};
-        String[] p2 = {"candy", "candy"};
+        List<List<String>> packets = new ArrayList<>();
+        List<String> p1 = List.of("candy", "candy");
+        List<String> p2 = List.of("candy", "candy");
         packets.add(p1);
         packets.add(p2);
         //Act
@@ -49,16 +37,84 @@ public class HalloweenShould {
     }
 
     @Test
-    void todo3() {
+    void return_trick_when_given_packet_contains_only_one_item(){
         //Arrange
-        List<String[]> packets = new ArrayList<>();
-        String[] p = {"candy", "apple"};
-        packets.add(p);
-        packets.add(p);
-        packets.add(p);
+        List<List<String>> packets = new ArrayList<>();
+        List<String> p1 = List.of("candy");
+        List<String> p2 = List.of("candy", "candy");
+        packets.add(p1);
+        packets.add(p2);
+        //Act
+        String actualResult = Hallowwen.givePacket(2 , packets);
+        //Assert
+        assertThat(actualResult).isEqualTo("Trick or treat!");
+    }
+    @Test
+    void return_trick_when_is_bomb_in_any_packets(){
+        //Arrange
+        List<List<String>> packets = new ArrayList<>();
+        List<String> p1 = List.of("candy", "candy", "bomb");
+        List<String> p2 = List.of("candy", "candy");
+        packets.add(p1);
+        packets.add(p2);
+        //Act
+        String actualResult = Hallowwen.givePacket(2 , packets);
+        //Assert
+        assertThat(actualResult).isEqualTo("Trick or treat!");
+
+    }
+
+    @Test
+    void return_trick_when_at_least_one_child_has_only_got_1_candy(){
+        //Arrange
+        List<List<String>> packets = new ArrayList<>();
+        List<String> p1 = List.of("candy", "candy");
+        List<String> p2 = List.of("candy", "candy");
+        List<String> p3 = List.of("candy","apple");
+        packets.add(p1);
+        packets.add(p2);
+        packets.add(p3);
         //Act
         String actualResult = Hallowwen.givePacket(3 , packets);
         //Assert
         assertThat(actualResult).isEqualTo("Trick or treat!");
     }
+    @Test
+    void return_Thx_when_children_has_same_number_of_candy(){
+        //Arrange
+        List<List<String>> packets = new ArrayList<>();
+        List<String> p1 = List.of("candy", "candy", "candy", "banana");
+        List<String> p2 = List.of("candy", "candy", "candy", "apple");
+        List<String> p3 = List.of("candy","candy", "candy");
+        packets.add(p1);
+        packets.add(p2);
+        packets.add(p3);
+        //Act
+        String actualResult = Hallowwen.givePacket(3 , packets);
+        //Assert
+        assertThat(actualResult).isEqualTo("Thank you, strange uncle!");
+    }
+
+    @Test
+    void return_trick_when_each_child_has_only_got_1_candy(){
+        //Arrange
+        List<List<String>> packets = new ArrayList<>();
+        List<String> p1 = List.of("candy", "apple");
+        List<String> p2 = List.of("candy", "apple");
+        List<String> p3 = List.of("candy","apple");
+        packets.add(p1);
+        packets.add(p2);
+        packets.add(p3);
+        //Act
+        String actualResult = Hallowwen.givePacket(3 , packets);
+        //Assert
+        assertThat(actualResult).isEqualTo("Trick or treat!");
+    }
+
+
+
+
+
+
+
 }
